9b9c9a9ad5feb1f2eb88ab848326e5d997cfd38e
Matt Freed <mwfreed@wpi.edu>
2019-02-21 22:36:24 -0500

    Update Reporting.java

--- Reporting.java
+++ Reporting.java
@@ -8,11 +8,10 @@ public class Reporting {
     public static void main(String[] argv) {
 
 
-	
         System.out.println("-------- Oracle JDBC Connection Testing ------");
         System.out.println("-------- Step 1: Registering Oracle Driver ------");
         try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
+            Class.forName("oracle.jdbc.driver.OracleDriver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return;
@@ -21,24 +20,21 @@ public class Reporting {
         System.out.println("Oracle JDBC Driver Registered Successfully !");
 
 
-	System.out.println("-------- Step 2: Building a Connection ------");
+        System.out.println("-------- Step 2: Building a Connection ------");
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(
-                                                     "jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl", "YOUR-Username”,
-                                                     "YOUR-SQL-Password");
-
+                    "jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl", "mwfreed",
+                    "MWFREED");
         } catch (SQLException e) {
             System.out.println("Connection Failed! Check output console");
             e.printStackTrace();
             return;
         }
-
         if (connection != null) {
             System.out.println("You made it. Connection is successful. Take control of your database now!");
         } else {
             System.out.println("Failed to make connection!");
         }
     }
-
 }
